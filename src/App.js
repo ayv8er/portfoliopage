267727025684b4970b49f5eafddcfc616e8d49c5
Richard Oh <n24512@protonmail.com>
2021-09-16 19:28:13 -0500

    added components, routes and switch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
+import GuestInput from "./components/GuestInput";
+
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <header>
-        <div>
-          <Link to="/home">Home</Link>
-        </div>
-        <div>
-          <Link to="/about">About</Link>
-        </div>
-        <div>
-          <Link to="/contact">Contact</Link>
-        </div>
-        <div>
-          <Link to="/projects">Projects</Link>
-        </div>
+        <nav className="navHead">
+          <div className="myName">
+            <h1>Richard J Oh</h1>
+          </div>
+          <div className="navigator">
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+          </div>
+        </nav>
       </header>
 
+      <div>
+        <GuestInput />
+      </div>
+
       <Switch>
-        <Route path="/projects" />
-        <Route path="/contact" />
-        <Route path="/about" />
-        <Route path="/home" />
-        <Route path="/" />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route path="/home" component={App} />
+        <Route path="/" component={App} />
       </Switch>
     </Router>
   );
---
+++ src/components/GuestInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const GuestInput = () => {
+  const [name, setName] = useState("Guest");
+
+  const addName = () => {
+    console.log("clicked");
+  };
+
+  const handleChange = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <>
+      <div>
+        <h3>Welcome {name} </h3>
+      </div>
+      <div>
+        <form onSubmit={addName}>
+          <label>
+            Please enter your name for a personalized experience:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={handleChange}
+            />
+          </label>
+          <div>
+            <button>Enter Name</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default GuestInput;
